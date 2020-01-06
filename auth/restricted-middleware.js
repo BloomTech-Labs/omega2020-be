ae835c0df56b89b97fbc245dd61d46d7641d9e31
Connor Sullivan <crsullivan@protonmail.com>
2020-01-06 12:45:46 -0700

    added auth

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,19 @@
+
+const jwt = require ('jsonwebtoken'); 
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const secret = process.env.JWT_SECRET || 'KEEP IT SECRET KEEP IT SAFE';
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'invalid credentials'})
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
