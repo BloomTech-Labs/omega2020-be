f6d172f0499a8ebf6ec9170866f420a2a1192c86
Connor Sullivan <crsullivan@protonmail.com>
2020-01-06 12:35:36 -0700

    building tables

--- database/migrations/20200106114827_omegadb.js
+++ database/migrations/20200106114827_omegadb.js
@@ -1,8 +1,50 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('user_puzzles', t => {
+        t.increments();
+        t
+        .integer('puzzle_id')
+        .unsigned()
+        .references('id')
+        .inTable('puzzles')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+        t
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+        t.time('time').notNullable();
+        t.string('difficulty', 128).notNullable();
+    })
+    .createTable('users', t => {
+        t.increments();
+        t.string('password', 128).notNullable();
+        t.string('email', 255).notNullable().unique();;
+    })
+    .createTable('settings', t => {
+        t.increments();
+        t
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+        t.string('theme', 255).notNullable();
+        t.boolean('text accessibility').notNullable();
+        t.boolean('dyslexic accessibility').notNullable();
+        t.boolean('sounds').notNullable();
+        t.boolean('notes').notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('settings')
+        .dropTableIfExists('users')
+        .dropTableIfExists('user_puzzles')
 };
