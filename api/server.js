264a3db180431d80f08c3de01fb9136f3270c302
Connor Sullivan <crsullivan@protonmail.com>
2020-01-07 13:50:15 -0700

    pg setup

--- api/server.js
+++ api/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const configureMiddleware = require('./configure-middleware')
 
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 // Update with your config settings.
-
 module.exports = {
 
   development: {
@@ -7,7 +7,13 @@ module.exports = {
     connection: {
       filename: './database/omega-db.db3'
     },
+    pool: {
+      afterCreate: (conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);
+      },
+  },
     useNullAsDefault: true,
+
     migrations: {
       directory: './database/migrations'
     },
@@ -15,7 +21,24 @@ module.exports = {
       directory: './database/seeds'
     }
   },
-
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+        filename: "./data/test.db3"
+    },
+    migrations: {
+        directory: './data/migrations'
+    },
+    seeds: {
+        directory: './data/seeds'
+    },
+    pool: {
+        afterCreate: (connection, done) => {
+            connection.run("PRAGMA foreign_keys = ON", done)
+        }
+    }
+},
   staging: {
     client: 'postgresql',
     connection: {
@@ -33,32 +56,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations'
   },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
+  seeds: {
+      directory: './database/seeds'
+  },
   },
 };
 
