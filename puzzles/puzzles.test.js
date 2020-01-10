2bccd602abfd1342041de286be944d08cf4661e0
Connor Sullivan <crsullivan@protonmail.com>
2020-01-10 12:18:37 -0700

    basic testing setup, 13 running, 13 passing

---
+++ puzzles/puzzles.test.js
@@ -0,0 +1,29 @@
+const superTest = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbconfig.js');
+
+const {
+    findPuzzle,
+    findPuzzles,
+    editPuzzle
+} = require('./puzzles-model')
+
+it('should set db env to testing', function() {
+    expect(process.env.DB_ENV).toBe("testing");
+})
+
+describe('GET', function() {
+    describe('GET endpoints return a proper response.', function() {
+        it('should return 200', function() {
+            return superTest(server).get('/puzzles').then(res => {
+                expect(res.status).toBe(200);
+            })
+        })
+        it('should return JSON formatted res', function() {
+            return superTest(server).get('/puzzles').then(res => {
+                expect(res.type).toMatch("application/json");
+            })
+        })
+    })
+
+})
