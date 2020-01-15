0434f7e87516bacc70fe4d1f6ea2dc9e3d546e33
Akak Almazbekova <akak.almazbekova@gmail.com>
2020-01-15 10:51:57 -0800

    postgres fix

--- users/users-model.js
+++ users/users-model.js
@@ -1,15 +1,11 @@
-  const db = require('../database/dbconfig.js');
+const db = require('../database/dbconfig.js');
 
 module.exports = {
     add,
     findBy,
     findById
 };
-// async function add(user) {
-//   const [id] = await db('users').insert(user);
 
-//   return findById(id);
-// }
 async function add(user) {
   if (process.env.DB_ENV === 'production') {
     return db.insert(user).into('users').returning('id')
@@ -27,12 +23,53 @@ async function add(user) {
   }
 }
 
-
 function findBy(email) {
     return db('users').where(email);
   }
 function findById(id) {
     return db('users')
-      .where({ id })
-      .first();
+    .where({id})
+    .first();
   }
+
+
+
+
+//   const db = require('../database/dbconfig.js');
+
+// module.exports = {
+//     add,
+//     findBy,
+//     findById
+// };
+// async function add(user) {
+//   const [id] = await db('users').insert(user);
+
+//   return findById(id);
+// }
+// async function add(user) {
+//   if (process.env.DB_ENV === 'production') {
+//     return db.insert(user).into('users').returning('id')
+//     .then(ids=> {
+//       console.log(ids[0]);
+//       return findById(ids[0]);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//       throw error;
+//     });
+//   } else {
+//     const [id] = await db('users').insert(user)
+//     return findById(id)
+//   }
+// }
+
+
+// function findBy(email) {
+//     return db('users').where(email);
+//   }
+// function findById(id) {
+//     return db('users')
+//       .where({ id })
+//       .first();
+//   }
