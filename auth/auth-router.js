d919e9b2056c8f68137434362220ab18d48a9641
Akak Almazbekova <akak.almazbekova@gmail.com>
2020-01-14 16:46:05 -0800

    Modifed database, auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,10 +7,9 @@ const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
     let user = req.body;
-
+  
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
-  
     Users.add(user)
       .then(saved => {
         res.status(201).json(saved);
--- users/users-model.js
+++ users/users-model.js
@@ -5,16 +5,25 @@ module.exports = {
     findBy,
     findById
 };
-async function add(user) {
-  const [id] = await db('users').insert(user);
 
-  return findById(id);
+async function add(user) {
+  if (process.env.DB_ENV === 'production') {
+    console.log('production')
+    return db('users').insert(user).returning('id')
+    .then(id=> findById(id))
+    .catch(error => {
+      throw error;
+    });
+  } else {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+  }
 }
 function findBy(email) {
     return db('users').where(email);
   }
 function findById(id) {
     return db('users')
-      .where({ id })
-      .first();
+    .where({'id': id})
+    .first();
   }
