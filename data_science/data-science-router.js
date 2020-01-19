64a2e17b7d50dec4296610df7863a493372c9d0d
Connor Sullivan <crsullivan@protonmail.com>
2020-01-19 11:55:01 -0700

    DS API
    Seem to be successfully pinging the Data Science API, however the data response is circular, and JavaScript seems to be at odds with that. this conmmit pings the server and returns the entire server response with util.inspection

--- data_science/data-science-router.js
+++ data_science/data-science-router.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
+const util = require('util')
 
-var mysql = require('mysql');
-
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    user: 'postgres',
-    password: 'omega2020database',
-    host: 'omega2020.cbydc0au6atn.us-east-2.rds.amazonaws.com',
-    database: 'postgres'
-})
-
+  host     : 'omega2020.cbydc0au6atn.us-east-2.rds.amazonaws.com',
+  user     : 'postgres',
+  password : 'omega2020database',
+  port     : '5432',
+});
+ 
 connection.connect(function(err) {
-    if (err) throw err;
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
 });
 
 router.get('/',(req,res)=> {
-    res.json('OK');
+    console.log(util.inspect(res))
+    // res.status(200).send(res);
 })
 
 module.exports = router;
