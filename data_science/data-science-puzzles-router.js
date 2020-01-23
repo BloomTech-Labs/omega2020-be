458072057da6b9d9c88fa8bedb480181905ecc14
Connor Sullivan <crsullivan@protonmail.com>
2020-01-23 10:44:47 -0700

    Testing Endpoint
    Adds an endpoint to provide the FE with a puzzle one input away from being solved, this is for the purpose of testing win state

--- data_science/data-science-puzzles-router.js
+++ data_science/data-science-puzzles-router.js
@@ -5,8 +5,8 @@ const connectionString = 'postgres://postgres:omega2020database@omega2020.cbydc0
 const pool = new Pool({
   connectionString: connectionString,
 })
-pool.query("SELECT sudoku, solution, level FROM puzzle_table WHERE level='Diabolical';", (err, res) => {
-  // console.log(res)
+pool.query("SELECT sudoku, solution, level, id FROM puzzle_table WHERE id='5029' LIMIT 1;", (err, res) => {
+  console.log(res.rows[0])
 })
 const client = new Client({
   connectionString: connectionString,
@@ -56,6 +56,15 @@ router.get('/moderate', (req, res, next) => {
 })
 router.get('/gentle', (req, res, next) => {
   pool.query("SELECT sudoku, solution, level, id FROM puzzle_table WHERE level='Gentle' ORDER BY RANDOM() LIMIT 1;",
+              (q_err, q_res ) => {
+                  res.json(q_res.rows[0])
+                  if (q_err) {
+                    res.send(q_err)
+                  }
+      })
+})
+router.get('/testing', (req, res, next) => {
+  pool.query("SELECT sudoku, solution, level, id FROM puzzle_table WHERE id='5029' LIMIT 1;",
               (q_err, q_res ) => {
                   res.json(q_res.rows[0])
                   if (q_err) {
