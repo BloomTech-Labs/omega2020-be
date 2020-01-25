8a5a8c4b64c7ce7e0d22b86892496d44338dda60
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-01-25 15:30:19 -0800

    seeded3

--- database/seeds/001_data.js
+++ database/seeds/001_data.js
@@ -1,11 +1,11 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('puzzles')
-    .then(function () {
-      // Inserts seed entries
-      return knex('puzzles').insert([
-        {id: 110, data: '.48....7...........732.651.7..4.5...3...7...8...8.3..5.541.736...........1....25.'}
-      ]);
-    });
-};
+// exports.seed = function(knex) {
+//   // Deletes ALL existing entries
+//   return knex('puzzles')
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('puzzles').insert([
+//         {id: 110, data: '.48....7...........732.651.7..4.5...3...7...8...8.3..5.541.736...........1....25.'}
+//       ]);
+//     });
+// };
