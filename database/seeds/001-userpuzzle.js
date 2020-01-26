d218cc2f4565bb9f0dab907676653902769f3477
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-01-26 10:46:24 -0800

    6th try with seeds
    debug ds connection for save

--- database/seeds/001-userpuzzle.js
+++ database/seeds/001-userpuzzle.js
@@ -5,9 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user_puzzles').insert([
-     { id: 100, difficulty: '1', data: '88888', user_id: 1, puzzleDs: 10, email: 'omega2021'},
-     { id: 101, difficulty: '2', data: '999999', user_id: 2, puzzleDs: 11, email: 'omega2022'},
-     { id: 102, difficulty: '3', data: '55555', user_id: 3, puzzleDs: 12, email: 'omega2023'}
+     { id: 100, difficulty: '1', data: '88888', user_id: 1, puzzleDs: 10, email: 'omega'}
       ]);   
     });
 };
