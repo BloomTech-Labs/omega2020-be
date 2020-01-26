61ee3b583158b1634488d382bdab7f0fab6860ba
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-01-26 10:24:32 -0800

    third try with seeds
    debug ds connection for save

--- database/seeds/001-userpuzzle.js
+++ database/seeds/001-userpuzzle.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('user-puzzles')
+  return knex('user_puzzles')
     .then(function () {
       // Inserts seed entries
-      return knex('user-puzzles').insert([
+      return knex('user_puzzles').insert([
      { id: 100, difficulty: '1', data: '88888', user_id: 1, puzzleDs: 10, email: 'omega2021', puzzle_id: 6},
      { id: 101, difficulty: '2', data: '999999', user_id: 2, puzzleDs: 11, email: 'omega2022', puzzle_id: 7},
      { id: 102, difficulty: '3', data: '55555', user_id: 3, puzzleDs: 12, email: 'omega2023', puzzle_id: 8}
