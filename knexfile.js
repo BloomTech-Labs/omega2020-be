bb24243d862f95578fab73e74ddeff6000b14d30
Tara Sherman <taradelin@gmail.com>
2020-05-22 22:44:19 -0400

    Rewrite knexfile for postgres

--- knexfile.js
+++ knexfile.js
@@ -1,51 +1,51 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  },
+	development: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		useNullAsDefault: true,
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	},
 
-  testing: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: process.env.TEST_DATABASE_URL,
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    }
-  },
+	testing: {
+		client: 'pg',
+		useNullAsDefault: true,
+		connection: process.env.TEST_DATABASE_URL,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		}
+	},
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  }
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		useNullAsDefault: true,
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	}
 };
