a4b3704be5b8f7006a96e6a95d8660a5b6bad8e3
Tara Sherman <taradelin@gmail.com>
2020-05-30 02:16:22 -0400

    Refactor knexfilefile for postgres from sqlite3

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
 	development: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		useNullAsDefault: true,
-		pool: {
-			min: 2,
-			max: 10
+		connection: {
+			host: process.env.HOST,
+			user: process.env.USER,
+			password: process.env.PASSWORD,
+			database: process.env.DATABASE
 		},
 		migrations: {
 			directory: './database/migrations'
@@ -16,30 +16,27 @@ module.exports = {
 			directory: './database/seeds'
 		}
 	},
-
 	testing: {
 		client: 'pg',
-		useNullAsDefault: true,
-		connection: process.env.TEST_DATABASE_URL,
+		connection: {
+			host: process.env.HOST,
+			user: process.env.USER,
+			password: process.env.PASSWORD,
+			database: process.env.TEST_DB
+		},
 		migrations: {
 			directory: './database/migrations'
 		},
 		seeds: {
 			directory: './database/seeds'
-		},
-		pool: {
-			min: 2,
-			max: 10
 		}
 	},
-
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		useNullAsDefault: true,
-		pool: {
-			min: 2,
-			max: 10
+		connection: {
+			connection: process.env.PRO_DB_URL || HOST,
+			user: process.env.USER,
+			password: process.env.PRO_PASS || PASSWORD
 		},
 		migrations: {
 			directory: './database/migrations'
