1a4acef2fdd72ac14bcf462c3da53da87047cbce
Tara Sherman <taradelin@gmail.com>
2020-06-10 22:29:59 -0400

    Refactored dbconfig for brevity

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,9 +1,3 @@
-const knex = require('knex');
-
-const knexConfig = require('../knexfile.js');
-
-const environment = 
-process.env.DB_ENV || 
-'production';
-
-module.exports = knex(knexConfig[environment]);
+const dbEngine = process.env.DB || 'dev'
+const config = require('../knexfile')[dbEngine]
+module.exports = require('knex')(config)
