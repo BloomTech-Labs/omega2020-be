31051f6348088b494deead9b27d79cb434cbacf1
Tara Sherman <taradelin@gmail.com>
2020-06-16 00:00:56 -0400

    Restored original tests Co-authored-by: Carlos Turcios <turcios50@gmail.com>

--- user_puzzles/user-puzzles.test.js
+++ user_puzzles/user-puzzles.test.js
@@ -2,8 +2,44 @@ const superTest = require('supertest');
 const server = require('../api/server');
 const db = require('../database/dbconfig.js');
 
-it('should set db env to testing', function() {
-    expect(process.env.DB_ENV).toBe("testing");
+// describe(
+//     'Test suite: register and login', () => {
+//         beforeAll(async () => {
+//             await db('users').truncate();
+//         });
+
+//         it('adds a user, returns JSON object', async () => {
+//             return res = await request(server)
+//                 .post('/auth/register')
+//                 .send({
+//                     "password": "testpass",
+//                     "email": "test@email.com"
+//                 })
+//                 .then(res => {
+//                     expect(res.status).toBe(201)
+//                     expect(res.type).toBe('application/json')
+//                     expect([res.body]).toBeArray()
+//                 })
+//         });
+
+//         it('should log in and return a token', async () => {
+//             return res = await request(server)
+//                 .post('/auth/login')
+//                 .send({
+//                     "password": "testpass",
+//                     "email": "test@email.com"
+//                 })
+//                 .then(res => {
+//                     expect(res.status).toBe(200)
+//                     expect(res.type).toBe('application.json')
+//                     expect(res.body.token).toBeTruthy();
+//                 })
+//                 );
+//         })
+
+
+it('should set db env to test', function() {
+    expect(process.env.DB_ENV).toBe("test");
 })
 
 describe('GET', function() {
