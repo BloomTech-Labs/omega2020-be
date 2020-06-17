72425c15a7eb09f028e3071a18097d1e90a3950f
Carlos Turcios <turcios50@gmail.com>
2020-06-16 18:35:32 -0700

    Co-authored-by: Tara Sherman <taradelin@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -1,63 +1,61 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: './database/omega-db.db3'
+      filename: './database/omega-db.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations'
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './database/seeds'
-    }
+      directory: './database/seeds',
+    },
   },
   testing: {
-    client: "sqlite3",
+    client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-        filename: "./database/test.db3"
+      filename: './database/test.db3',
     },
     migrations: {
-        directory: './database/migrations'
+      directory: './database/migrations',
     },
     seeds: {
-        directory: './database/seeds'
+      directory: './database/seeds',
     },
     pool: {
-        afterCreate: (connection, done) => {
-            connection.run("PRAGMA foreign_keys = ON", done)
-        }
-    }
-},
+      afterCreate: (connection, done) => {
+        connection.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations'
-  },
-  seeds: {
-      directory: './database/seeds'
-  },
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 };
-
--- users/users-model.js
+++ users/users-model.js
@@ -1,46 +1,46 @@
 const db = require('../database/dbconfig.js');
 
 module.exports = {
-    add,
-    findBy,
-    findById
+  add,
+  findBy,
+  findById,
 };
 
 async function add(user) {
   if (process.env.DB_ENV === 'production') {
-    console.log(db.insert(user).into('users').returning('id'), "add method")
-    return db.insert(user).into('users').returning('id')
-    .then(ids=> {
-      console.log(ids[0]);
-      return findById(ids[0]);
-    })
-    .catch(error => {
-      console.log(error);
-      throw error;
-    });
+    console.log(db.insert(user).into('users').returning('id'), 'add method');
+    return db
+      .insert(user)
+      .into('users')
+      .returning('id')
+      .then((ids) => {
+        console.log(ids[0]);
+        return findById(ids[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+        throw error;
+      });
   } else {
-    console.log(user)
-    const [id] = await db('users').insert(user)
-    console.log()
-    return findById(id)
+    console.log(user);
+    const [id] = await db('users').insert(user);
+    console.log();
+    return findById(id);
   }
 }
 
 function findBy(email) {
-    return db('users').where(email);
-  }
+  return db('users').where(email);
+}
 function findById(id) {
-    return db('users')
-    .where({id})
+  return db('users')
+    .where({ id })
     .first()
-    .catch(error=> {
-      console.log("Error finding by id");
+    .catch((error) => {
+      console.log('Error finding by id');
       throw error;
     });
-  }
-
-
-
+}
 
 //   const db = require('../database/dbconfig.js');
 
@@ -70,7 +70,6 @@ function findById(id) {
 //     return findById(id)
 //   }
 // }
-
 
 // function findBy(email) {
 //     return db('users').where(email);
