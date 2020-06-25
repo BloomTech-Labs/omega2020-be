65f3901f33e479b3c1876c45748cfc6eef88a534
Tara Sherman <taradelin@gmail.com>
2020-06-24 20:30:06 -0400

    Tests: 2 failed, 8 passed

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -4,74 +4,81 @@ const Users = require('../users/users-model.js');
 const request = require('supertest');
 const server = require('../api/server');
 
-// describe('Test suite: register and login', () => {
-// 	beforeAll(async () => {
-// 		await db('users').truncate();
-// 	});
+// function resetTable(...tables) {
+// 	const query = tables
+// 		.map((table) => `TRUNCATE TABLE users RESTART IDENTITY CASCADE;`)
+// 		.join('');
+// 	return db.raw(query);
+// }
 
-// 	it('adds a user, returns JSON object', async () => {
-// 		return (res = await request(server)
-// 			.post('/auth/register')
-// 			.send({
-// 				password: 'testpass',
-// 				email: 'test@email.com'
-// 			})
-// 			.then((res) => {
-// 				expect(res.status).toBe(201);
-// 				expect(res.type).toBe('application/json');
-// 				expect([res.body]).toBeArray();
-// 			}));
-// 	});
+describe('Test suite: register and login', () => {
+	beforeAll(async () => {
+		await db.raw('TRUNCATE TABLE users RESTART IDENTITY CASCADE');
+	});
 
-// 	it('should log in and return a token', async () => {
-// 		return (res = await request(server)
-// 			.post('/auth/login')
-// 			.send({
-// 				password: 'testpass',
-// 				email: 'test@email.com'
-// 			})
-// 			.then((res) => {
-// 				expect(res.status).toBe(200);
-// 				expect(res.type).toBe('application/json');
-// 				expect(res.body.token).toBeTruthy();
-// 			}));
-// 	});
-// });
+	it('adds a user, returns JSON object', async () => {
+		return (res = await request(server)
+			.post('/auth/register')
+			.send({
+				password: 'testpass',
+				email: 'test@email.com'
+			})
+			.then((res) => {
+				expect(res.status).toBe(201);
+				expect(res.type).toBe('application/json');
+				expect([res.body]).toBeArray();
+			}));
+	});
 
-it('should set db env to test', function() {
-    expect(process.env.NODE_ENV).toBe("test");
-})
+	it('should log in and return a token', async () => {
+		return (res = await request(server)
+			.post('/auth/login')
+			.send({
+				password: 'testpass',
+				email: 'test@email.com'
+			})
+			.then((res) => {
+				expect(res.status).toBe(200);
+				expect(res.type).toBe('application/json');
+				expect(res.body.token).toBeTruthy();
+			}));
+	});
+});
 
-describe('add()', function() {
-    beforeEach(async () => {
-        await db('users').truncate();
-    })
-    it('should insert a user', async function() {
-        await add({password: 'bendoe', email: 'ben123@doe.com'});
-        const users = await db('users');
-        expect(users).toHaveLength(1);
-    })
-    it('should return provided user', async function() {
-        user = await add({password: 'johndoe', email: 'john123@doe.com'});
-        expect(user.email).toBe('john123@doe.com');
-        expect(user.id).toBeDefined();
+// it('should set db env to test', function() {
+//     expect(process.env.NODE_ENV).toBe("test");
+// })
 
-    })
-})
+// describe('add()', function() {
+//     beforeEach(async () => {
+//         await db('users').truncate();
+//     })
+//     it('should insert a user', async function() {
+//         await add({password: 'bendoe', email: 'ben123@doe.com'});
+//         const users = await db('users');
+//         expect(users).toHaveLength(1);
+//     })
+//     it('should return provided user', async function() {
+//         user = await add({password: 'johndoe', email: 'john123@doe.com'});
+//         expect(user.email).toBe('john123@doe.com');
+//         expect(user.id).toBeDefined();
 
-describe('findBy()', function() {
-    beforeEach(async () => {
-        await db('users').truncate();
-    })
-    it('should find a user', async function() {
-        original = await add({password: 'johndoe', email: 'john@doe.com'});
-        user = await Users.findBy({email: 'john@doe.com'})
-        expect(user).toBeDefined();
-        expect(user).toMatchObject([original]);
-    })
-    it('should return the correct user', async function() {
-        original = await add({password: 'johndoe', email: 'john@doe.com'});
-        user = await Users.findBy({email: 'john@doe.com'})
-        expect(user).toMatchObject([original]);
-    })
-})
+//     })
+// })
+
+// describe('findBy()', function() {
+//     beforeEach(async () => {
+//         await db('users').truncate();
+//     })
+//     it('should find a user', async function() {
+//         original = await add({password: 'johndoe', email: 'john@doe.com'});
+//         user = await Users.findBy({email: 'john@doe.com'})
+//         expect(user).toBeDefined();
+//         expect(user).toMatchObject([original]);
+//     })
+//     it('should return the correct user', async function() {
+//         original = await add({password: 'johndoe', email: 'john@doe.com'});
+//         user = await Users.findBy({email: 'john@doe.com'})
+//         expect(user).toMatchObject([original]);
+//     })
+// })
