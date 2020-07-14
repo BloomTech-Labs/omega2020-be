1b0e05443e8290e456f0b096c9851a2aae6bd85c
Tara Sherman <taradelin@gmail.com>
2020-07-13 23:59:13 -0400

    TEST: endpoints returning JSON object/status 200

--- data_science/data-science-puzzles.test.js
+++ data_science/data-science-puzzles.test.js
@@ -11,23 +11,97 @@ const client = new Client({
 
 client.connect();
 
+//Test GET request for random 9x9 puzzle
 describe('Test suite: receive correct response from database for random puzzle', () => {
   afterAll(async () => {
     db.end();
   });
 
-  it('returns an object with status 200', async () => {
+  it('returns a JSON object with status 200', async () => {
     return (res = await request(server)
       .get('/puzzle')
       .then(res => {
         expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+      }))
+  });
+});
+
+//Test GET request for easy 4x4 puzzle
+describe('Test suite: receive correct response from database for a 4x4 puzzle, difficulty level = easy', () => {
+  afterAll(async () => {
+    db.end();
+  });
+
+  it('returns an easy 4x4 puzzle with status 200', async () => {
+    return (res = await request(server)
+      .get('/puzzle/4x4/easy')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
       }))
   })
+});
 
-  it('returns a JSON object', async () => {
+//Test GET request for easy 6x6 puzzle
+describe('Test suite: receive correct response from database for a 6x6 puzzle, difficulty level = easy', () => {
+  afterAll(async () => {
+    db.end();
+  });
+
+  it('returns an easy 6x6 puzzle with status 200', async () => {
     return (res = await request(server)
-      .get('/puzzle')
+      .get('/puzzle/6x6/easy')
       .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+      }))
+  })
+});
+
+//Test GET request for easy 9x9 puzzle
+describe('Test suite: receive correct response from database for a 9x9 puzzle, difficulty level = easy', () => {
+  afterAll(async () => {
+    db.end();
+  });
+
+  it('returns an easy 9x9 puzzle with status 200', async () => {
+    return (res = await request(server)
+      .get('/puzzle/9x9/easy')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+      }))
+  })
+});
+
+//Test GET request for medium 9x9 puzzle
+describe('Test suite: receive correct response from database for a 9x9 puzzle, difficulty level = medium', () => {
+  afterAll(async () => {
+    db.end();
+  });
+
+  it('returns an medium 9x9 puzzle with status 200', async () => {
+    return (res = await request(server)
+      .get('/puzzle/9x9/medium')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+      }))
+  })
+});
+
+//Test GET request for hard 9x9 puzzle
+describe('Test suite: receive correct response from database for a 9x9 puzzle, difficulty level = hard', () => {
+  afterAll(async () => {
+    db.end();
+  });
+
+  it('returns an easy 9x9 puzzle with status 200', async () => {
+    return (res = await request(server)
+      .get('/puzzle/9x9/hard')
+      .then(res => {
+        expect(res.status).toBe(200);
         expect(res.type).toBe('application/json');
       }))
   })
