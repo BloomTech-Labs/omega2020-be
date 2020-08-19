cab77dddc603bd9a7bf2090c6a06806906abfffd
Tara Sherman <taradelin@gmail.com>
2020-08-18 22:11:45 -0400

    Removed corsOptions

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -4,22 +4,24 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// Unsuccessful attempt to correct cors error
+
 // Set up a whitelist and check against it:
-const whitelist = ['https://www.sudomega.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = ['https://www.sudomega.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 module.exports = server => {
   server.use(express.json());  
   server.use(cors());
-	server.use(cors(corsOptions));
+	// server.use(cors(corsOptions));
   server.use(helmet());
   // server.use(morgan('dev'));
     server.use(bodyParser());
