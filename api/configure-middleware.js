200fa95bc1ab2834de0718dcd83b55b72dd44933
Tara Sherman <taradelin@gmail.com>
2020-08-18 21:44:24 -0400

    Added corsOptions

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 // const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// Set up a whitelist and check against it:
+const whitelist = ['https://www.sudomega.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 module.exports = server => {
-  server.use(express.json());
+  server.use(express.json());  
+  // server.use(cors());
+	server.use(cors(corsOptions));
   server.use(helmet());
   // server.use(morgan('dev'));
-  server.use(cors());
-  server.use(bodyParser());
+    server.use(bodyParser());
 };
