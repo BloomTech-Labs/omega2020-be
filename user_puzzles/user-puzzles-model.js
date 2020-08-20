038595b0578995e6ddceab93c5c19d6b3c5798b2
Tara Sherman <taradelin@gmail.com>
2020-08-19 23:13:55 -0400

    POST and GET userPuzzles working locally Co-authored-by: Carlos Turcios <turcios50@gmail.com>

--- user_puzzles/user-puzzles-model.js
+++ user_puzzles/user-puzzles-model.js
@@ -7,13 +7,34 @@ function findUserPuzzles(email) {
 		.select();
 }
 
-async function saveUserPuzzle(userPuzzle) {
-	const [id] = await db('user_puzzles').insert(userPuzzle)
-	return db('user_puzzles')
-		.where({ id })
-		.where({ email:email })
-		.first()
-};
+async function saveUserPuzzle(puzzle, email, puzzleId, original) {
+	await db('user_puzzles')
+		.insert({
+			...puzzle,
+			email: email,
+			puzzleDs: puzzleId,
+			original: original
+		})
+		.catch((err) => {
+			console.log(err);
+			throw err;
+		});
+	const savedPuzzle = {
+		...puzzle,
+		email,
+		puzzleId,
+		original
+	};
+	return savedPuzzle;
+}
+
+// async function saveUserPuzzle(userPuzzle) {
+// 	const [email] = await db('user_puzzles').insert(userPuzzle)
+// 	return db('user_puzzles')
+// 		// .where({ id })
+// 		.where({ email: email })
+// 		.first()
+// };
 
 module.exports = {
 	findUserPuzzles,
@@ -22,25 +43,7 @@ module.exports = {
 	// getUserPuzzles
 };
 
-// async function savePuzzle(puzzle, email, puzzleId, original) {
-// 	await db('user_puzzles')
-// 		.insert({
-// 			...puzzle,
-// 			email: email,
-// 			puzzleDs: puzzleId,
-// 			original: original
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 			throw err;
-// 		});
-// 	const savedPuzzle = {
-// 		...puzzle,
-// 		email,
-// 		puzzleId,
-// 		original
-// 	};
-// 	return savedPuzzle;
+
 
 // function findUserPuzzleByID(id) {
 // 	return db('user_puzzles').where({ puzzleID }).first();
